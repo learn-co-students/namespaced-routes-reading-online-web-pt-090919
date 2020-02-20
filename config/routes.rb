bce6c88c46605d927a881d8075c6093d48c387a2
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-02-20 13:12:10 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,20 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  # get '/admin/stats', to: 'stats#index'
+  # get '/admin/authors/new', to: 'authors#new'
+  # get '/admin/authors/delete', to: 'authors#delete'
+  # get '/admin/authors/create', to: 'authors#create'
+  # get '/admin/comments/moderate', to: 'comments#moderate'
 
+  # Prefix a block of routes under one grouping /admin 
+  # Tell scope to use /admin as URL prefix
+  # Routes will be handled by controllers in admin module
+  # scope '/admin', module 'admin' do
+  # use namespace to route with a module & use its name as  URL prefix
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+  
   root 'posts#index'
 end
